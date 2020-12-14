2f2428772a9f6462ca69f2bf6b9241f5b39d7cb9
xmas1256 <xmas59233@gmail.com>
2020-12-14 18:17:56 +0500

    first

--- update.py
+++ update.py
@@ -57,6 +57,4 @@ dp.add_handler(MessageHandler(Filters.forwarded, forward))
 updater.start_webhook(listen='0.0.0.0',
                       port=8443,
                       url_path=TOKEN,
-                      key='private.key',
-                      cert='cert.pem',
                       webhook_url='https://example.com:8443/TOKEN')
